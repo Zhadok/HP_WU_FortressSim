a3a2a60abf5ba79d64bbdd310588ec2533e73838
Zhadok <johnyare@gmail.com>
2019-09-30 12:13:33 +0200

    Auto-generated commit

--- src/model/player/SkillTree/PersistedSkillTreeNode.ts
+++
@@ -1,9 +0,0 @@
-import { triggerNameType, statNameType } from "../../../types";
-
-export interface PersistedSkillTreeNode {
-
-    readonly rowIndex: number;
-    readonly columnIndex: number; 
-    readonly levelStudied: number 
-
-}
--- ui/README.md
+++
@@ -1,2 +0,0 @@
-# Harry Potter Wizards Unite: Fortress Simulator Frontned
-This frontend was developed with the Angular 8 framework. 
